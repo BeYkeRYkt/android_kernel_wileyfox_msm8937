8c673fba43c64713496890471c58060f5cfb4c89
Mark Rutland <mark.rutland@arm.com>
2016-03-09 14:08:18 -0800

    sched/kasan: remove stale KASAN poison after hotplug
    Functions which the compiler has instrumented for KASAN place poison on
    the stack shadow upon entry and remove this poision prior to returning.
    
    In the case of CPU hotplug, CPUs exit the kernel a number of levels deep
    in C code.  Any instrumented functions on this critical path will leave
    portions of the stack shadow poisoned.
    
    When a CPU is subsequently brought back into the kernel via a different
    path, depending on stackframe, layout calls to instrumented functions
    may hit this stale poison, resulting in (spurious) KASAN splats to the
    console.
    
    To avoid this, clear any stale poison from the idle thread for a CPU
    prior to bringing a CPU online.
    
    Change-Id: I05a91c3e39e86e186e957cb84442a7da456e8fb7
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Reviewed-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Reviewed-by: Ingo Molnar <mingo@kernel.org>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Git-commit: e1b77c92981a522223bd1ac118fdcade6b7ad086
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Signed-off-by: Mohammed Khajapasha <mkhaja@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -26,6 +26,7 @@
  *              Thomas Gleixner, Mike Kravetz
  */
 
+#include <linux/kasan.h>
 #include <linux/mm.h>
 #include <linux/module.h>
 #include <linux/nmi.h>
@@ -8479,6 +8480,8 @@ void init_idle(struct task_struct *idle, int cpu)
 
 	idle->state = TASK_RUNNING;
 	idle->se.exec_start = sched_clock();
+
+	kasan_unpoison_task_stack(idle);
 
 	do_set_cpus_allowed(idle, cpumask_of(cpu));
 	/*
