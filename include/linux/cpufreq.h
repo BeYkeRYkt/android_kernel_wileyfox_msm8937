bfd932b8d0e6be2efa170a316dd9b12507709c5a
BeYkeRYkt <beykerykt@gmail.com>
2021-11-28 02:01:38 +0900

    include: cpufreq: Declare cpufreq_task_stats_remove_uids as static inline
    Change-Id: Ie86ab713b9e288a8ea3078b9669700b467c75a21

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -675,6 +675,7 @@ static inline void cpufreq_task_stats_init(struct task_struct *p) {}
 static inline void cpufreq_task_stats_alloc(struct task_struct *p) {}
 static inline void cpufreq_task_stats_free(struct task_struct *p) {}
 static inline void cpufreq_task_stats_exit(struct task_struct *p) {}
+static inline void cpufreq_task_stats_remove_uids(uid_t uid_start, uid_t uid_end) {}
 
 #endif
 
